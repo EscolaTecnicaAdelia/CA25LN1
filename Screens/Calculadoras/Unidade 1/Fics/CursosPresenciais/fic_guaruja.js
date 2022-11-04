4b2d7675e61332fa3d5495a4c07284f2de677e6f
JoãoVitorFerreiro <jvferreiro1@gmail.com>
2022-11-04 17:36:52 -0300

    first commit

---
+++ Screens/Calculadoras/Unidade 1/Fics/CursosPresenciais/fic_guaruja.js
@@ -0,0 +1,71 @@
+var desconto183 = 59.33;
+var desconto225 =  50;
+var desconto283 = 37;
+var desconto360 = 20;
+var TecPresencial="- Entrada de R$99,00 (Noventa e Nove Reais) no ato da matrícula + 7 parcelas mensais. |- Material Didático Incluso";
+
+
+var cursos = [{
+    cod: 1,	
+    curso:"Presencial",
+    categoria: 1,
+    valorIntegral: 450,
+    PlanoDePagamento: TecPresencial,
+    SegundaMensalidade: "Até o dia 1º R$462,96 | Até o dia 7º R$486,11 | Até o dia 25º R$578,71",
+    desconto: desconto183,
+},
+{
+    cod: 2,	
+    curso:"Presencial",
+    categoria: 2,
+    valorIntegral: 450,
+    PlanoDePagamento: TecPresencial,
+    SegundaMensalidade: "Até o dia 1º R$462,96 | Até o dia 7º R$486,11 | Até o dia 25º R$578,71",
+    desconto: desconto360,
+},
+{
+    cod: 3,	
+    curso:"Presencial",
+    categoria: 3,
+    valorIntegral: 450,
+    PlanoDePagamento: TecPresencial,
+    SegundaMensalidade: "Até o dia 1º R$462,96 | Até o dia 7º R$486,11 | Até o dia 25º R$578,71",
+    desconto: desconto225,
+},
+{
+    cod: 4,	
+    curso:"Presencial",
+    categoria: 4,
+    valorIntegral: 450,
+    PlanoDePagamento: TecPresencial,
+    SegundaMensalidade: "Até o dia 1º R$462,96 | Até o dia 7º R$486,11 | Até o dia 25º R$578,71",
+    desconto: desconto225,
+},
+{
+    cod: 5,	
+    curso:"Presencial",
+    categoria: 5,
+    valorIntegral: 450,
+    PlanoDePagamento: TecPresencial,
+    SegundaMensalidade: "Até o dia 1º R$462,96 | Até o dia 7º R$486,11 | Até o dia 25º R$578,71",
+    desconto: desconto283,
+},
+{
+    cod: 6,	
+    curso:"Presencial",
+    categoria: 6,
+    valorIntegral: 450,
+    PlanoDePagamento: TecPresencial,
+    SegundaMensalidade: "Até o dia 1º R$462,96 | Até o dia 7º R$486,11 | Até o dia 25º R$578,71",
+    desconto: desconto283,
+},
+{
+    cod: 7,	
+    curso:"Presencial",
+    categoria: 7,
+    valorIntegral: 450,
+    PlanoDePagamento: TecPresencial,
+    SegundaMensalidade: "Até o dia 1º R$462,96 | Até o dia 7º R$486,11 | Até o dia 25º R$578,71",
+    desconto: desconto183,
+},
+]
---
+++ Screens/Calculadoras/Unidade 2/Fics/CursosEAD/fic_vc.js
@@ -0,0 +1,76 @@
+var desconto183 = 59.33;
+var desconto283 = 37;
+var desconto360 = 20;
+var TecPresencial="- Entrada de R$99,00 (Noventa e Nove Reais) no ato da matrícula + 7 parcelas mensais. |- Material Didático Incluso";
+var ParcelasPresencial="Até o Dia 1º";
+
+
+var cursos = [{
+    cod: 1,	
+    curso:"EAD",
+    categoria: 1,
+    SegundaMensalidade: "- Organização Empresarial |- Gestão de Pessoas |- Métodos e Técnicas Administrativas |- Informática Aplicada |- Redação Oficial",
+},
+{
+    cod: 2,	
+    curso:"EAD",
+    categoria: 2,
+    SegundaMensalidade: "- Economia e Mercado |- Contabilidade Geral |- Matemática Financeira |- Informática Aplicada |- Direito público privado, legislação trabalhista e previdenciária",
+},
+{
+    cod: 3,	
+    curso:"EAD",
+    categoria: 3,
+    SegundaMensalidade: "- Gestão da Cadeia de Suprimentos |- Gestão de Compras e Logística Internacional |-Logística de Armazenagem |- Logística de Transporte |- Escrita Fiscal e Legislação Tributária",
+},
+{
+    cod: 4,	
+    curso:"EAD",
+    categoria: 4,
+    SegundaMensalidade: "- Gestão de Pessoas |- Métodos e Técnicas Administrativas |- Saúde e Segurança do Trabalho |- Português Instrumental |- Informática Aplicada",
+},
+{
+    cod: 5,	
+    curso:"EAD",
+    categoria: 5,
+    SegundaMensalidade: "- Dispensação Farmacêutica |- Farmacotécnica |- Legislação Farmacêutica |- Biossegurança em Saúde |- Atendimento Humanizado em Saúde",
+},
+{
+    cod: 6,	
+    curso:"EAD",
+    categoria: 6,
+    SegundaMensalidade: "- Eletricidade Básica |- Instalações Elétricas |- Geração, Transmissão e Distribuição de Energia Elétrica |- Máquinas e Equipamentos |- Saúde e Segurança do Trabalho",
+},
+{
+    cod: 7,	
+    curso:"EAD",
+    categoria: 7,
+    SegundaMensalidade: "- Técnicas de Maquiagem |- Técnicas de Maquiagem Avançadas para Delineado |- Planejamento de Marketing |- Empreendedorismo |- Informática Aplicada",
+},
+{
+    cod: 8,	
+    curso:"EAD",
+    categoria: 8,
+    SegundaMensalidade: "- Fundamentos de Hardware |- Manutenção de Computadores |- Sistemas Operacionais Livres |- Redes de Computadores |- Empreendedorismo",
+},
+{
+    cod: 9,	
+    curso:"EAD",
+    categoria: 9,
+    SegundaMensalidade: "- Lógica da Programação |- Banco de Dados |- Estrutura de Dados |- Engenharia de Software |- Programação Orientada a Objetos",
+},
+{
+    cod: 10,	
+    curso:"EAD",
+    categoria: 10,
+    SegundaMensalidade: "- Atendimento Humanizado em Saúde |- Português Instrumental |- Informática Aplicada |- Biossegurança em Saúde |- Primeiros Socorros",
+},
+
+{   
+    cod: 11,	
+    curso:"EAD",
+    categoria: 11,
+    SegundaMensalidade: "- Técnicas de Negociação e Vendas |- Gestão Comercial |- Planejamento de Marketing |- Informática Aplicada |- Português Instrumental",
+},
+
+]
